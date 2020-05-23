7831133246eb8cefaeb20b69dfbd8f6de4ee4071
Glitch (ninth-stirring-ton) <none>
2020-05-23 02:12:15 +0000

    🙅🙅 Updated with Glitch

--- src/confirmation.js
+++ src/confirmation.js
@@ -1,7 +1,7 @@
-const axios = require('axios');
-const qs = require('qs');
+const axios = require("axios");
+const qs = require("qs");
 
-const apiUrl = 'https://slack.com/api';
+const apiUrl = "https://slack.com/api";
 
 /*
  *  Send confirmation via chat.postMessage to the user who clipped it
@@ -11,53 +11,71 @@ const apiUrl = 'https://slack.com/api';
 // DM works for the user installed the app, and only after other user manually add the app
 // We are still investing the issue
 
-const sendConfirmation = (userId, view) => {
-  let values = view.state.values;
-
-  let blocks = [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: 'Message clipped!\n\n'
-      }
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: `*Message*\n${values.message.message_id.value}`
-      }
-    },
-    {
-      type: 'section',
-      fields: [
-        {
-          type: 'mrkdwn',
-          text: `*Importance:*\n${values.importance.importance_id.selected_option.text.text}`
-        },
-        {
-          type: 'mrkdwn',
-          text: `*Link:*\nhttp://example.com/${userId}/clip`
-        }
-      ]
-    }
-  ];
-
+async function getUserInfo(userId) {
   let message = {
     token: process.env.SLACK_ACCESS_TOKEN,
-    channel: userId,
-    blocks: JSON.stringify(blocks)
+    user: userId
   };
+  let username = "anon";
+  let myiconurl = undefined;
+  let result = await axios.post(`${apiUrl}/users.info`, qs.stringify(message));
 
-  axios.post(`${apiUrl}/chat.postMessage`, qs.stringify(message))
-  .then((result => {
-    console.log(result.data);
-  }))
-  .catch((err) => {
-    console.log(err);
-  });
+  username = result.data.user.profile.display_name;
+  if (username === undefined) {
+    username = result.data.user.profile.real_name;
+  }
+  if (username === undefined) {
+    username = result.data.user.name;
+  }
+  return {
+    image_original: result.data.user.profile.image_original,
+    image_24: result.data.user.profile.image_24,
+    username: username
+  };
 }
 
+const sendConfirmation = (userId, view, channelid, origuserid) => {
+  let values = view.state.values;
+  Promise.all([getUserInfo(userId), getUserInfo(origuserid)]).then(users => {
+    let blocks = [
+      {
+        type: "context",
+        elements: [
+          {
+            type: "image",
+            image_url: users[1].image_24,
+            alt_text: "usericon"
+          },
+          {
+            type: "mrkdwn",
+            text: "*" + users[1].username + "*"
+          }
+        ]
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: values.message.message_id.value
+        }
+      }
+    ];
+    let message = {
+      token: process.env.SLACK_ACCESS_TOKEN,
+      channel: channelid,
+      blocks: JSON.stringify(blocks),
+      username: users[0].username,
+      icon_url: users[0].image_original
+    };
+    axios
+      .post(`${apiUrl}/chat.postMessage`, qs.stringify(message))
+      .then(result => {
+        console.log(result.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+};
 
 module.exports = { sendConfirmation };
--- src/index.js
+++ src/index.js
@@ -36,7 +36,8 @@ app.use(bodyParser.json({ verify: rawBodyBuffer }));
 /* To use actions and dialogs, enable the Interactive Components in your dev portal.
 /* Scope: `command` to enable actions
  */
-
+let channelid = 0
+let origuserid = 0
 app.post('/actions', (req, res) => { 
 
   const payload = JSON.parse(req.body.payload);
@@ -51,7 +52,8 @@ app.post('/actions', (req, res) => {
 
   // Message action triggered
   if(type === 'message_action') {
- 
+    channelid = payload.channel.id
+    origuserid = payload.message.user
     // Open a modal with the selected message pre-populated
     openModal(payload).then((result) => {
       if(result.data.error) {
@@ -69,7 +71,7 @@ app.post('/actions', (req, res) => {
     res.send(''); // Make sure to respond immediately to the Slack server to avoid an error
     
     // DM the user a confirmation message
-    confirmation.sendConfirmation(user.id, view);
+    confirmation.sendConfirmation(user.id, view,channelid, origuserid);
    
   }  
   
@@ -87,12 +89,12 @@ const openModal = async(payload) => {
       type: 'modal',
       title: {
         type: 'plain_text',
-        text: 'Save it to ClipIt!'
+        text: 'Better reply'
       },
       callback_id: 'clipit',
       submit: {
         type: 'plain_text',
-        text: 'ClipIt'
+        text: 'Reply'
       },
       blocks: [
         {
@@ -102,56 +104,14 @@ const openModal = async(payload) => {
             action_id: 'message_id',
             type: 'plain_text_input',
             multiline: true,
-            initial_value: payload.message.text
+            initial_value:'> ' + payload.message.text + '\n'
           },
           label: {
             type: 'plain_text',
             text: 'Message Text'
           }
-        },
-        {
-          block_id: 'importance',
-          type: 'input',
-          element: {
-            action_id: 'importance_id',
-            type: 'static_select',
-            placeholder: {
-              type: 'plain_text',
-              text: 'Select importance',
-              emoji: true
-            },
-            options: [
-              {
-                text: {
-                  type: 'plain_text',
-                  text: 'High 💎💎✨',
-                  emoji: true
-                },
-                value: 'high'
-              },
-              {
-                text: {
-                  type: 'plain_text',
-                  text: 'Medium 💎',
-                  emoji: true
-                },
-                value: 'medium'
-              },
-              {
-                text: {
-                  type: 'plain_text',
-                  text: 'Low ⚪️',
-                  emoji: true
-                },
-                value: 'low'
-              }
-            ]
-          },
-          label: {
-            type: 'plain_text',
-            text: 'Importance'
-          }
         }
+        
       ]
     })
   };
